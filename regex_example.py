ac15f0a8badbc602dac70f9f9f0cdc155b33ad92
Nicholas McGlincy <nmcglincy@zymergen.com>
2020-05-27 09:52:10 -0700

    Update regex_example.py
    Notes on regex lectures

--- regex_example.py
+++ regex_example.py
@@ -10,3 +10,13 @@ print(phoneNumRegex.findall(message))
 # greedy matching - looking for the longest possible string that matches the pattern.
 # digitRegex = re.compile(r"(\d){3,5}?")
 # is a non-greedy match
+# python has its own shortcut characters, a caret within square brackets inverts the selection
+# e.g. [^aeiou] matches everything that is not a lower case vowel.
+# greedy match
+# re.compile(r'.*')
+# everything including spaces
+# re.compile(r'.*', re.DOTALL)
+# non-greedy match
+# re.compile(r'.*?')
+# re.compile(r'[aeiou]', re.I)
+# re.compile(r'[aeiou]', re.IGNORECASE)
